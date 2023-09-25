12bc17bf956d133ce6ca1cfb4bb1c68f7992edb5
niloofar <sn.mousavi77@gmail.com>
2023-09-25 16:26:42 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      weather app
-      <footer>
-        this project was coded by Niloofar Mousavi and is {""}
-        <a
-          href="https://github.com/niloofar77/weather-app-react"
-          target="_blank"
-        >
-          open-sourced by Github
-        </a>
-      </footer>
+      <div className="container">
+        weather app
+        <footer>
+          this project was coded by Niloofar Mousavi and is {""}
+          <a
+            href="https://github.com/niloofar77/weather-app-react"
+            target="_blank"
+          >
+            open-sourced by Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
