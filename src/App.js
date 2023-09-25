b2cd77ac15710894f553b852099dc75c6db1e449
niloofar <sn.mousavi77@gmail.com>
2023-09-25 15:33:33 +0330

    added Github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      weather app
+      <footer>
+        this project was coded by Niloofar Mousavi and is {""}
+        <a
+          href="https://github.com/niloofar77/weather-app-react"
+          target="_blank"
+        >
+          open-sourced by Github
+        </a>
+      </footer>
+    </div>
+  );
 }
